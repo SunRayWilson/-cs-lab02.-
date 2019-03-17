056dd9b19078110a72185c2b1c786324bc2c455c
Alice (PotylikoNA) <alice@example.com>
2019-03-18 01:23:13 +0300

    build:максимум

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-int a, b;
+int a, b, max;
 cin >> a >> b;
 cout  << "A - B = " << a - b << '\n'
 << "A + B = " << a + b << '\n'
 << "A * B = " << a * b << '\n'
-<< "A / B = " << a / b << '\n'
-
-    ;
+<< "A / B = " << a / b << '\n' ;
+if(a>b)
+max=a;
+else
+max=b;
 
 
 }
