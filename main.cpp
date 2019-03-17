d5710d7d4c2a576e28b875a11b79ef993d35664b
Bob (PotylikoNA) <bob@example.com>
2019-03-17 16:14:41 +0300

    build: изменение типа

--- main.cpp
+++ main.cpp
@@ -13,11 +13,13 @@ cout  << "A - B = " << a - b << '\n'
 << "A + B = " << a + b << '\n'
 << "A * B = " << a * b << '\n'
 << "A / B = " << a / b << '\n' ;
+
 if(a>b)
 max=a;
 else
 max=b;
 cout<<"Max="<<max;
+
 if (a< b)
    min=a;
 else
